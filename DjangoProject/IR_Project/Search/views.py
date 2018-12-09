707640cfdcdd1707bd66872dad20d7d75d84b57b
anuragiiits <anurag.iiits@gmail.com>
2018-12-09 15:41:03 +0530

    Upload and Downlaod files finished

--- DjangoProject/IR_Project/Search/views.py
+++ DjangoProject/IR_Project/Search/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.conf import settings
 from django.core.files.storage import FileSystemStorage
+from django.contrib import messages
 
 import os
 import subprocess
@@ -18,6 +19,13 @@ def upload_page(request):
         myfile = request.FILES['file']
         fs = FileSystemStorage()
         filename = fs.save(myfile.name, myfile)
+        try:
+            op = subprocess.check_output('java -jar /home/anurag/Documents/java/Lucene-based-Hindi-Search-Engine/DjangoProject/IR_Project/Search/CompiledJarFile/CreateIndex.jar', shell=True).decode("UTF-8")
+        except subprocess.CalledProcessError as e:
+            messages.warning(request, "There is an error")
+            print("command '{}' return with error (code {}): {}".format(e.cmd, e.returncode, e.output))
+        # op = str(op)
+        # os.system('java -jar /home/anurag/Documents/django/IR_Project/Search/CompiledJarFile/CreateIndex.jar')
         return render(request, 'home.html')
 
 def search_page(request):
@@ -28,21 +36,25 @@ def search_page(request):
         text = request.POST.get('search_text')
         # s = "पास प्यासे के कुआँ आता नहीं है"
         # s1 = "rabindranath"
-        op = subprocess.check_output('java -jar /home/anurag/Documents/django/IR_Project/Search/CompiledJarFile/SearchModel.jar '+text, shell=True).decode("UTF-8")
-        op = str(op)
-        lines = op.splitlines()
-        count = int(lines[0].split(' ')[-1])
-        print(count)
-        contents = []
-        if count >= 1:
-            paths = [(lines[i][18:lines[i].find(',')],lines[i].split(' ')[-1]) for i in range(1, len(lines))]
-            for i in range(1, len(lines)):
-                path = lines[i][7:lines[i].find(',')].replace("\\","/")
-                fp = open(path, 'r')
-                contents.append([next(fp) for x in range(5)])
-                fp.close()
-
-        # print(contents)
-        paths = list(zip(paths, contents))
+        paths = []
+        try:
+            op = subprocess.check_output('java -jar /home/anurag/Documents/java/Lucene-based-Hindi-Search-Engine/DjangoProject/IR_Project/Search/CompiledJarFile/SearchModel.jar '+text, shell=True).decode("UTF-8")
+            op = str(op)
+            lines = op.splitlines()
+            count = int(lines[0].split(' ')[-1])
+            print(count)
+            contents = []
+            paths = []
+            if count >= 1:
+                paths = [(lines[i][18:lines[i].find(',')],lines[i].split(' ')[-1]) for i in range(1, len(lines))]
+                for i in range(1, len(lines)):
+                    path = lines[i][7:lines[i].find(',')].replace("\\","/")
+                    fp = open(path, 'r')
+                    contents.append([next(fp) for x in range(5)])
+                    fp.close()
+                paths = list(zip(paths, contents))
+        except subprocess.CalledProcessError as e:
+            messages.warning(request, "There is an error")
+            print("command '{}' return with error (code {}): {}".format(e.cmd, e.returncode, e.output))
         # print("Paths", paths)
         return render(request, 'results.html', {"results": paths})
